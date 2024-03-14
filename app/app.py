90c17a10ab063b2ec5ce722a17585a7b62df0f49
JasonGellis <jasonjgellis@gmail.com>
2024-03-14 17:17:18 +0000

    add agreparse to app.py

--- app/app.py
+++ app/app.py
@@ -1,17 +1,17 @@
-from utils import read_images, convert_to_grayscale, normalize_images, perform_ocr, process_text,pad_columns, create_dataframe
+import argparse
+from read_and_process import read_images, convert_to_grayscale, normalize_images, \
+    perform_ocr, process_text,pad_columns, create_dataframe
 
 
-def main():
-    # Main function to orchestrate the application
-    read_images()
-    convert_to_grayscale()
-    normalize_images()
-    perform_ocr()
-    process_text()
-    pad_columns()
-    create_dataframe()
-
+if __name__ == '__main__':
+    # read and process images
+    images = read_images
+    grayscale_images = convert_to_grayscale(images)
+    normalized_images = normalize_images(grayscale_images)
+    extracted_text = perform_ocr(normalized_images)
+    processed_data = process_text(extracted_text)
+    padded_data = pad_columns(processed_data)
+    df = create_dataframe(padded_data)
+  
     
-# Entry point
-if __name__ == "__main__":
-    main()
+    # main(args.input_dir, args.output_dir)

57fdd28c3cd460d8c80a1764ed720a04150d21ac
JasonGellis <jasonjgellis@gmail.com>
2024-03-17 16:32:54 +0000

    cleanup code add docstrings

--- app.py
+++ app.py
@@ -1,54 +1,52 @@
-"""Module for running argument parser and running read_and_process."""
-import argparse # import packages
-import config
-from shared_utils.read_and_process import read_images, convert_to_grayscale, \
-    normalize_images, perform_ocr, process_text, pad_columns, remove_special_characters, \
-    create_dataframe, save_dataframe_to_directory
+""" Module for table reader main function and argparse"""
 
-# Initialize config variables
-DEFAULT_INPUT_DIR = "/path/to/default_input_directory"
-DEFAULT_OUTPUT_DIR = "/path/to/default_output_directory"
+import argparse
+
+from cli.read_and_process import read_images, convert_to_grayscale, \
+    normalize_images, perform_ocr, process_text, pad_columns, \
+        remove_special_characters, create_dataframe, save_dataframe_to_directory
+
+import config
 
 def parse_arguments():
     """
     Parse command-line arguments.
     """
     parser = argparse.ArgumentParser(description="Process tables and output data frames")
-    parser.add_argument(
-    "-i",
-    "--input-dir",
-    help="Path to input directory",
-    default=config.DEFAULT_INPUT_DIR,
-    required=True
-    )
-    parser.add_argument(
-    "-o", 
-    "--output-dir", 
-    help="Path to output directory",
-    default=config.DEFAULT_OUTPUT_DIR,
-    required=True)
+    parser.add_argument("-i", "--input-dir",
+                        help="Path to input directory",
+                        default=config.DEFAULT_INPUT_DIR,
+                        required=True)
+    parser.add_argument("-o", "--output-dir",
+                        help="Path to output directory",
+                        default=config.DEFAULT_OUTPUT_DIR,
+                        required=True)
     args = parser.parse_args()
     return args
 
 def main():
-    """
-    Main function for processing images and generating output CSV file.
 
-    This function serves as the entry point for the application. It parses command-line
-    arguments, sets input and output directories, processes images, performs OCR,
-    processes extracted text, creates a DataFrame, and saves the DataFrame to a CSV file.
+    """
+    Process images and text files according to specified input and output directories.
+
+    Parses command-line arguments to set input and output directories. Then, reads images
+    from the input directory, converts them to grayscale, normalizes them, performs OCR
+    to extract text, processes the extracted text, pads columns, removes special characters,
+    creates a DataFrame, and finally saves the DataFrame to the output directory as 'output.csv'.
 
     Args:
-        None
+        None (Uses command-line arguments for input and output directories)
 
     Returns:
         None
     """
+
     args = parse_arguments()
 
     # Set input and output directories in config module
     config.set_input_directory(args.input_dir)
     config.set_output_directory(args.output_dir)
+
     # functions from read_and_process
     images = read_images(args.input_dir)
     grayscale_images = convert_to_grayscale(images)

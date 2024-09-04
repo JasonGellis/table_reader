884791a44178e4e8b6f0d6067f7a03fda5e944b8
JasonGellis <jasonjgellis@gmail.com>
2024-09-04 15:18:36 +0100

    update setup for CLI install

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
-""" App setup file. """
-
 from setuptools import setup, find_packages
 
 with open("requirements.txt", "r") as f:
     REQUIRED_PACKAGES = f.read().splitlines()
 
 setup(
-    name="Table Reader",
-    version="1.0",
-    description="A python app for extracting data from images",
+    name="table_reader",
+    version="1.0.0",
+    description="A Python app for extracting data from images",
     download_url="https://github.com/JasonGellis/table_reader",
     author="Jason Jacob Gellis",
     author_email="jg760@cam.ac.uk",
     license="MIT",
-    keywords=['data extraction', 'data analysis', \
-        'optical character recognition', 'computer vision'],
+    keywords=[
+        'data extraction', 'data analysis',
+        'optical character recognition', 'computer vision'
+    ],
     install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'tablereader=app:main',  # This is where you define the CLI command and the entry point
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
